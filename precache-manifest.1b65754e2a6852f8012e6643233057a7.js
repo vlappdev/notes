66efa3bdfe39a55c75a84324ff86e5e690624b9d
vlappdev <vladimir.eoffice@gmail.com>
2020-06-27 22:31:58 +0200

    Updates

---
+++ precache-manifest.1b65754e2a6852f8012e6643233057a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3b126eb4b6e8d3f57b638a4db7b00f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4514c11b6ac58ca825d",
+    "url": "/static/css/main.fcd6b830.chunk.css"
+  },
+  {
+    "revision": "9e60e901d26c6d8c0a46",
+    "url": "/static/js/2.dfbdc59e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfbdc59e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4514c11b6ac58ca825d",
+    "url": "/static/js/main.f66d7c13.chunk.js"
+  },
+  {
+    "revision": "b6b7328ed845ec8d67bd",
+    "url": "/static/js/runtime-main.e8880884.js"
+  }
+]);
